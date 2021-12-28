1939db6aee86aa73f816f8bca8cfc774cc5b2411
g3kgirl <shikha.relieved@gmail.com>
2021-12-28 17:40:20 +0530

    initial commit

---
+++ src/component/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        fontSize: "30px",
+        margin: "40px",
+      }}
+    >
+      About Page
+    </div>
+  );
+};
+
+export default About;
---
+++ src/component/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        fontSize: "30px",
+        margin: "40px",
+      }}
+    >
+      contact with us
+    </div>
+  );
+};
+
+export default Contact;
